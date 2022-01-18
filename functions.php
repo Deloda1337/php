1edac489160cb8594fdbf49494530a8ecd817afa
Deloda1337 <97990314+Deloda1337@users.noreply.github.com>
2022-01-19 01:08:14 +0300

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,45 +1,70 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
+<?php
 
-<?php 
+include "db.php";
 
-function init(){
+function getUsersId () {
+    global $connection;
+    $new_query = "SELECT * FROM users";
+    $result = mysqli_query($connection, $new_query);
+        while ($row = mysqli_fetch_assoc($result)) {
+        $id = $row['Id'];
+        echo "<option value='$id'>$id</option>";
 
+        }
+};
 
-say_Something();
-echo "<br>";
-calculate();  
+function createUser () {
+    global $connection;
+    if (isset($_POST["create_new_user"])) {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $new_query = "INSERT INTO users(Username, Password)";
+        $new_query .= "VALUES ('$username','$password')";
+        $result = mysqli_query($connection, $new_query);
+    }
+};
+
+function updateUser () {
+    if (isset($_POST['update_data'])) {
+        global $connection;
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $id = $_POST['id'];
+        $query = "UPDATE users SET ";
+        $query .= "Username = '$username', ";
+        $query .= "Password = '$password' ";
+        $query .= "WHERE Id = $id";
+        $result = mysqli_query($connection, $query);
+        if (!$result) {
+            die('die' . mysqli_error($connection));
+        }
+    }
+}
+
+function showUsers () {
+    if (isset($_POST["show_data_db"])) {
+        $new_query = "SELECT * FROM users";
+        global $connection;
+        $result = mysqli_query($connection, $new_query);
+        while ($row = mysqli_fetch_assoc($result)) {
+            print_r($row);
+        }
+    }
+
 
 }
 
-
-
-
-function calculate(){
-
-    echo 456 + 345;
-
+function deleteUser () {
+    if (isset($_POST['delete_user'])) {
+        global $connection;
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $id = $_POST['id'];
+        $query = "DELETE FROM users ";
+        $query .= "WHERE Id = $id";
+        $result = mysqli_query($connection, $query);
+        if (!$result) {
+            die('die' . mysqli_error($connection));
+        }
+    }
 }
-
-
-function say_Something(){
-
-   echo "Hello Student, do you like the class? yes? okay great";
-
-}
-
-  init();  
-    
-    
-    
-?>
-
-
-
-</body>
-</html>
